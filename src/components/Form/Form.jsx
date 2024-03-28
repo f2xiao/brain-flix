e0f35bda67247cdd1eb369d6f9c7f022d1f74601
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-03-28 17:31:45 -0400

    add Form component

---
+++ src/components/Form/Form.jsx
@@ -0,0 +1,27 @@
+import "./Form.scss";
+import Avatar from "../Avatar/Avatar";
+import Button from "../Button/Button";
+import avatarUrl from "../../assets/images/Mohan-muruge.jpg";
+import commentUrl from "../../assets/icons/add_comment.svg";
+
+const Form = () => {
+  return (
+    <form className="form">
+      <Avatar className="form__avatar" url={avatarUrl} />
+      <div className="form__body">
+        <div className="form__input">
+          <label className="form__label">Join the conversation</label>
+          <input
+            className="form__textarea"
+            type="textarea"
+            placeholder="Add a new comment"
+            required
+          ></input>
+        </div>
+        <Button text="comment" iconUrl={commentUrl} />
+      </div>
+    </form>
+  );
+};
+
+export default Form;
