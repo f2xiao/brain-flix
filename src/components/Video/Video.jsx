63507cb96312eeb40767e3717fe6622474fb1520
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-03-30 16:38:28 -0400

    extract VideoDetails component from Video component

--- src/components/Video/Video.jsx
+++ src/components/Video/Video.jsx
@@ -1,23 +1,6 @@
 import "./Video.scss";
-import Comment from "../../components/Comment/Comment";
-import Form from "../../components/Form/Form";
-import viewsIcon from "../../assets/icons/views.svg";
-import likesIcon from "../../assets/icons/likes.svg";
-import Stat from "../Stat/Stat";
 
-const Video = ({ videoObj }) => {
-  const {
-    title,
-    channel,
-    timestamp,
-    views,
-    likes,
-    description,
-    image,
-    video,
-    comments,
-  } = videoObj;
-
+const Video = ({ image, video }) => {
   return (
     <section className="video">
       <div className="video__wrapper">
@@ -26,30 +9,6 @@ const Video = ({ videoObj }) => {
             video
           </video>
         </div>
-      </div>
-
-      <div className="video__copy">
-        <h1>{title}</h1>
-        <div className="video__subheader">
-          <h2 className="video__channel">{channel}</h2>
-          <p className="video__date ">
-            {new Date(timestamp).toLocaleDateString()}
-          </p>
-          <Stat iconUrl={viewsIcon} name={views} />
-          <p className="video__date video__date--second">
-            {new Date(timestamp).toLocaleDateString()}
-          </p>
-          <Stat iconUrl={likesIcon} name={likes} />
-        </div>
-        <p className="video__description">{description}</p>
-      </div>
-
-      <div className="video__comments">
-        <h3>{comments.length} Comments</h3>
-        <Form />
-        {comments.map((commentObj) => (
-          <Comment key={commentObj.id} commentObj={commentObj} />
-        ))}
       </div>
     </section>
   );
---
+++ src/components/VideoDetails/VideoDetails.jsx
@@ -0,0 +1,40 @@
+import Comment from "../../components/Comment/Comment";
+import Form from "../../components/Form/Form";
+import viewsIcon from "../../assets/icons/views.svg";
+import likesIcon from "../../assets/icons/likes.svg";
+import Stat from "../Stat/Stat";
+import "./VideoDetails.scss";
+
+const VideoDetails = ({ videoObj }) => {
+  const { title, channel, timestamp, views, likes, description, comments } =
+    videoObj;
+  return (
+    <div className="video-details">
+      <div className="video-details__copy">
+        <h1>{title}</h1>
+        <div className="video-details__subheader">
+          <h2 className="video-details__channel">{channel}</h2>
+          <p className="video-details__date ">
+            {new Date(timestamp).toLocaleDateString()}
+          </p>
+          <Stat iconUrl={viewsIcon} name={views} />
+          <p className="video-details__date video-details__date--second">
+            {new Date(timestamp).toLocaleDateString()}
+          </p>
+          <Stat iconUrl={likesIcon} name={likes} />
+        </div>
+        <p className="video-details__description">{description}</p>
+      </div>
+
+      <div className="video-details__comments">
+        <h3>{comments.length} Comments</h3>
+        <Form />
+        {comments.map((commentObj) => (
+          <Comment key={commentObj.id} commentObj={commentObj} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default VideoDetails;
