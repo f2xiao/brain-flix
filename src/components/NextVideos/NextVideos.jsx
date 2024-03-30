d8eb5bf6f8372052917c4b1bac50455bf1d7cf7d
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-03-30 14:53:07 -0400

    add props to NextVideos component

--- src/components/NextVideos/NextVideos.jsx
+++ src/components/NextVideos/NextVideos.jsx
@@ -1,32 +1,19 @@
 import "./NextVideos.scss";
 
-const NextVideos = () => {
+const NextVideos = ({ nextVidoes }) => {
   return (
     <div className="next-videos">
       <h3 className="next-videos__title">NEXT VIDEOS</h3>
       <ul className="next-videos__list">
-        <li className="next-videos__item">
-          <img
-            className="next-videos__img"
-            src="https://unit-3-project-api-0a5620414506.herokuapp.com/images/image0.jpg"
-            alt="title"
-          />
-          <div className="next-videos__text">
-            <h3>title</h3>
-            <p>channel</p>
-          </div>
-        </li>
-        <li className="next-videos__item">
-          <img
-            className="next-videos__img"
-            src="https://unit-3-project-api-0a5620414506.herokuapp.com/images/image0.jpg"
-            alt="title"
-          />
-          <div className="next-videos__text">
-            <h3>title</h3>
-            <p>channel</p>
-          </div>
-        </li>
+        {nextVidoes.map(({ id, title, channel, image }) => (
+          <li key={id} className="next-videos__item">
+            <img className="next-videos__img" src={image} alt={title} />
+            <div className="next-videos__text">
+              <h3>{title}</h3>
+              <p>{channel}</p>
+            </div>
+          </li>
+        ))}
       </ul>
     </div>
   );
