2422f17bf1792af366c2c4530cf8c28ae609062b
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-03-30 15:11:19 -0400

    add wrapper container

--- src/components/NextVideos/NextVideos.jsx
+++ src/components/NextVideos/NextVideos.jsx
@@ -3,18 +3,20 @@ import "./NextVideos.scss";
 const NextVideos = ({ nextVidoes }) => {
   return (
     <div className="next-videos">
-      <h3 className="next-videos__title">NEXT VIDEOS</h3>
-      <ul className="next-videos__list">
-        {nextVidoes.map(({ id, title, channel, image }) => (
-          <li key={id} className="next-videos__item">
-            <img className="next-videos__img" src={image} alt={title} />
-            <div className="next-videos__text">
-              <h3>{title}</h3>
-              <p>{channel}</p>
-            </div>
-          </li>
-        ))}
-      </ul>
+      <div className="next-videos__wrapper">
+        <h3 className="next-videos__title">NEXT VIDEOS</h3>
+        <ul className="next-videos__list">
+          {nextVidoes.map(({ id, title, channel, image }) => (
+            <li key={id} className="next-videos__item">
+              <img className="next-videos__img" src={image} alt={title} />
+              <div className="next-videos__text">
+                <h3>{title}</h3>
+                <p>{channel}</p>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
