fc6b027bbd7959493772417c3a7c41fe2f3bfc47
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-04-02 17:13:39 -0400

    add routes to the app component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.scss";
+import VideoDetailsPage from "./pages/VideoDetailsPage/VideoDetailsPage";
+import UploadPage from "./pages/UploadPage/UploadPage";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<VideoDetailsPage />} />
+        <Route path="videos/:videoId" element={<VideoDetailsPage />} />
+        <Route path="upload" element={<UploadPage />} />
+        <Route path="*" element={<>Not Found 404</>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
