8fc532cbad903cc0b5177f42dd85d514e97aa2ef
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-04-04 19:46:43 -0400

    render Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.scss";
 import VideoDetailsPage from "./pages/VideoDetailsPage/VideoDetailsPage";
 import UploadPage from "./pages/UploadPage/UploadPage";
+import Header from "./components/Header/Header";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<VideoDetailsPage />} />
         <Route path="videos/:videoId" element={<VideoDetailsPage />} />
