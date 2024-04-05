5dbe2074bc0acf1afd49eab15572dfae7fc3c751
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-04-04 21:05:21 -0400

    update styles of not found 404 page

--- src/App.js
+++ src/App.js
@@ -6,13 +6,16 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter className="app">
       <Header />
       <Routes>
         <Route path="/" element={<VideoDetailsPage />} />
         <Route path="videos/:videoId" element={<VideoDetailsPage />} />
         <Route path="upload" element={<UploadPage />} />
-        <Route path="*" element={<>Not Found 404</>} />
+        <Route
+          path="*"
+          element={<div className="app__wrapper">Not Found 404 🥲</div>}
+        />
       </Routes>
     </BrowserRouter>
   );
