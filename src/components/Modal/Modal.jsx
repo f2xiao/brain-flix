2a07469e809a71b1f7eeedc7adcb82903289f8aa
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-04-08 10:16:46 -0400

    add modal component

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,13 @@
+import "./Modal.scss";
+
+const Modal = () => {
+  return (
+    <div className="modal">
+      <div className="modal__body">
+        <p>Publish successfully, redirecting to the homepage</p>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
