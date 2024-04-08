d29091f94e2c1c1cb23c43481341e7e7023a66df
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-04-08 10:17:30 -0400

    update UploadForm component to render Modal conditionally

--- src/components/UploadForm/UploadForm.jsx
+++ src/components/UploadForm/UploadForm.jsx
@@ -3,65 +3,75 @@ import videoThumbnailUrl from "../../assets/images/Upload-video-preview.jpg";
 import publishUrl from "../../assets/icons/publish.svg";
 import Button from "../../components/Button/Button";
 import { Link, useNavigate } from "react-router-dom";
+import Modal from "../../components/Modal/Modal";
+import { useState } from "react";
 
 const UploadForm = () => {
+  const [showModal, setShowModal] = useState(false);
   const navigate = useNavigate();
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    alert("Upload successfully, redirect to home page");
-    navigate("/");
+    setShowModal(true);
+    setTimeout(() => {
+      navigate("/");
+    }, 3000);
   };
   return (
-    <div className="upload">
-      <h1>Upload Video</h1>
-      <form className="upload__form" onSubmit={handleSubmit}>
-        <div className="upload__thumbnail">
-          <label htmlFor="" className="upload__label">
-            VIDEO THUMBNAIL
-          </label>
-          <img
-            className="upload__image"
-            src={videoThumbnailUrl}
-            alt="video thumbnail"
-          />
+    <>
+      {showModal ? (
+        <Modal />
+      ) : (
+        <div className="upload">
+          <h1>Upload Video</h1>
+          <form className="upload__form" onSubmit={handleSubmit}>
+            <div className="upload__thumbnail">
+              <label htmlFor="" className="upload__label">
+                VIDEO THUMBNAIL
+              </label>
+              <img
+                className="upload__image"
+                src={videoThumbnailUrl}
+                alt="video thumbnail"
+              />
+            </div>
+            <div className="upload__data">
+              <label htmlFor="title" className="upload__label">
+                TITLE YOUR VIDEO
+              </label>
+              <input
+                placeholder="Add a title to your video"
+                type="text"
+                className="upload__input"
+                required
+              />
+              <label
+                htmlFor="description"
+                className="upload__label"
+                placeholder="Add a title to your video"
+              >
+                ADD A VIDEO DESCRIPTION
+              </label>
+              <textarea
+                className="upload__textarea"
+                placeholder="Add a description to your video"
+                required
+              ></textarea>
+            </div>
+            <div className="upload__cta">
+              <Link to="/">
+                <button className="upload__button">Cancel</button>
+              </Link>
+              <Button text="Publish" iconUrl={publishUrl} />
+              <Link to="/">
+                <button className="upload__button upload__button--second">
+                  Cancel
+                </button>
+              </Link>
+            </div>
+          </form>
         </div>
-        <div className="upload__data">
-          <label htmlFor="title" className="upload__label">
-            TITLE YOUR VIDEO
-          </label>
-          <input
-            placeholder="Add a title to your video"
-            type="text"
-            className="upload__input"
-            required
-          />
-          <label
-            htmlFor="description"
-            className="upload__label"
-            placeholder="Add a title to your video"
-          >
-            ADD A VIDEO DESCRIPTION
-          </label>
-          <textarea
-            className="upload__textarea"
-            placeholder="Add a description to your video"
-            required
-          ></textarea>
-        </div>
-        <div className="upload__cta">
-          <Link to="/">
-            <button className="upload__button">Cancel</button>
-          </Link>
-          <Button text="Publish" iconUrl={publishUrl} />
-          <Link to="/">
-            <button className="upload__button upload__button--second">
-              Cancel
-            </button>
-          </Link>
-        </div>
-      </form>
-    </div>
+      )}
+    </>
   );
 };
 
