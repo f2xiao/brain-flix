70e0bbf8f7fdf11948560a304a6b53f3879016e0
f2xiao <25191617+f2xiao@users.noreply.github.com>
2024-07-31 16:59:48 -0400

    update default comment username

--- src/components/VideoDetails/VideoDetails.jsx
+++ src/components/VideoDetails/VideoDetails.jsx
@@ -24,7 +24,7 @@ const VideoDetails = ({ currentVideoId }) => {
     // console.log(newComment);
     // post the comment to /videos/:id/comments
     const newCommentObj = {
-      name: "Vivi",
+      name: "Fangfang",
       comment: newComment,
     };
 
